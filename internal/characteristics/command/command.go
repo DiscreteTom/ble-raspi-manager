2fed711e2395cec2c1696d707a88b1b8e0d54d0b
DiscreteTom <discrete_tom@outlook.com>
2022-03-29 20:43:26 +0800

    adopt new transport layer for command char

--- internal/characteristics/command/command.go
+++ internal/characteristics/command/command.go
@@ -1,57 +1,37 @@
 package command
 
 import (
-	"encoding/json"
 	"github/DiscreteTom/ble-raspi-manager/internal/shell"
+	"github/DiscreteTom/ble-raspi-manager/internal/transport"
 
 	"github.com/google/uuid"
 	"tinygo.org/x/bluetooth"
 )
 
-type command struct {
-	UUID   string
-	Cmd    string
-	Output string
-}
-
 func NewCharacteristicConfig(serviceUUID uuid.UUID) bluetooth.CharacteristicConfig {
 	cmdCharUUID := uuid.NewSHA1(serviceUUID, []byte("cmd"))
 	cmdCharBleUUID, _ := bluetooth.ParseUUID(cmdCharUUID.String())
 
-	currentCmd := &command{}
-	readOutputFrom := 0
+	var reader *transport.ReadHandler
+	writer := transport.NewWriteHandler(func(uuid, content []byte) {
+		go func() {
+			output, err := shell.RunCommand(string(content))
+			if err != nil {
+				output = "Error: " + err.Error()
+			}
+
+			reader = transport.NewReadHandler(uuid, []byte(output))
+		}()
+	})
 
 	return bluetooth.CharacteristicConfig{
 		UUID:  cmdCharBleUUID,
 		Flags: bluetooth.CharacteristicWritePermission | bluetooth.CharacteristicReadPermission,
 		ReadEvent: func(client bluetooth.Connection) ([]byte, error) {
-			uuid := []byte(currentCmd.UUID)
-			output := []byte(currentCmd.Output)
-			if readOutputFrom < len(output) {
-				end := readOutputFrom + 256
-				if end > len(output) {
-					end = len(output)
-				}
-				result := append(uuid, output[readOutputFrom:end]...)
-				readOutputFrom += 256 // send 256 byte every time
-				return result, nil
-			} else {
-				return uuid, nil // only send uuid when finish
-			}
+			return reader.Read(), nil
 		},
 		WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
-			newCmd := &command{}
-			json.Unmarshal(value, &newCmd)
-			go func() {
-				output, err := shell.RunCommand(newCmd.Cmd)
-				if err != nil {
-					output = "Error: " + err.Error()
-				}
-				currentCmd.Output = output
-				currentCmd.UUID = newCmd.UUID
-				currentCmd.Cmd = newCmd.Cmd
-				readOutputFrom = 0
-			}()
+			writer.Write(value)
 		},
 	}
 }
