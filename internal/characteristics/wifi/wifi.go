141f64ef913d44686fabc7d0304bab55ab010646
DiscreteTom <discrete_tom@outlook.com>
2022-03-30 07:46:18 +0800

    update wifi - allow multi wifi - use new transport protocol

--- internal/characteristics/wifi/wifi.go
+++ internal/characteristics/wifi/wifi.go
@@ -4,29 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"github/DiscreteTom/ble-raspi-manager/internal/shell"
+	"github/DiscreteTom/ble-raspi-manager/internal/transport"
+	"strings"
 
 	"github.com/google/uuid"
 	"tinygo.org/x/bluetooth"
 )
 
 type wifiInfo struct {
-	SSID      string // wifi name
-	PSK       string // wifi password
-	CurrentIP string // current ip address
-	Router    string
-	Static    bool   // whether ip is static
-	StaticIP  string // configured static ip
+	SSID string // wifi name
+	PSK  string // wifi password
 }
 
-func getWifiInfo() wifiInfo {
-	staticIP := getStaticIP()
-	return wifiInfo{
-		SSID:      getSSID(),
-		PSK:       "password can NOT be retrieved",
-		CurrentIP: getCurrentIP(),
-		Static:    len(staticIP) != 0,
-		StaticIP:  staticIP,
-		Router:    getRouter(),
+type request struct {
+	RefreshOnly bool
+	WIFIs       []*wifiInfo
+	StaticIP    string
+	Router      string
+}
+
+type state struct {
+	CurrentWIFI string // current wifi ssid
+	CurrentIP   string // current ip address
+	Router      string
+	StaticIP    string // configured static ip
+
+	WIFIs []*wifiInfo
+}
+
+func getState() state {
+	return state{
+		CurrentWIFI: getCurrentWIFI(),
+		CurrentIP:   getCurrentIP(),
+		Router:      getRouter(),
+		StaticIP:    getStaticIP(),
+		WIFIs:       getWifiInfo(),
 	}
 }
 
@@ -42,8 +54,22 @@ func getCurrentIP() string {
 	return getCommandOutput("ifconfig wlan0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\\.){3}[0-9]*).*/\\2/p'")
 }
 
-func getSSID() string {
-	return getCommandOutput("cat /etc/wpa_supplicant/wpa_supplicant.conf | grep -Eo 'ssid=\"[^\"]*\"' | cut -d'\"' -f 2")
+func getWifiInfo() []*wifiInfo {
+	ssids := strings.Split(getCommandOutput("cat /etc/wpa_supplicant/wpa_supplicant.conf | grep -Eo 'ssid=\"[^\"]*\"' | cut -d'\"' -f 2"), "\n")
+	result := []*wifiInfo{}
+	for _, ssid := range ssids {
+		if len(ssid) != 0 {
+			result = append(result, &wifiInfo{
+				SSID: ssid,
+				PSK:  "",
+			})
+		}
+	}
+	return result
+}
+
+func getCurrentWIFI() string {
+	return getCommandOutput("iwgetid -r")
 }
 
 func getStaticIP() string {
@@ -54,14 +80,16 @@ func getRouter() string {
 	return getCommandOutput("netstat -nr | awk '$1 == \"0.0.0.0\"{print$2}'")
 }
 
-func setNewWifi(ssid, psk string) {
-	if len(getSSID()) != 0 {
+func setWifiInfo(wifis []*wifiInfo) {
+	if len(getWifiInfo()) != 0 {
 		// remove original settings
 		shell.MustRunCommand("cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.backup")
 		shell.MustRunCommand("cat /etc/wpa_supplicant/wpa_supplicant.conf.backup | grep -v 'network=' | grep -v 'ssid=' | grep -v 'psk=' | grep -v '}' > /etc/wpa_supplicant/wpa_supplicant.conf")
 	}
 	// write new settings
-	shell.MustRunCommand(fmt.Sprintf("wpa_passphrase '%s' '%s' | grep -v '#psk' >> /etc/wpa_supplicant/wpa_supplicant.conf", ssid, psk))
+	for _, wifi := range wifis {
+		shell.MustRunCommand(fmt.Sprintf("wpa_passphrase '%s' '%s' | grep -v '#psk' >> /etc/wpa_supplicant/wpa_supplicant.conf", wifi.SSID, wifi.PSK))
+	}
 	shell.MustRunCommand("wpa_cli -i wlan0 reconfigure") // restart interface to apply
 }
 
@@ -88,26 +116,52 @@ func NewCharacteristicConfig(serviceUUID uuid.UUID) bluetooth.CharacteristicConf
 	wifiCharUUID := uuid.NewSHA1(serviceUUID, []byte("wifi"))
 	wifiCharBleUUID, _ := bluetooth.ParseUUID(wifiCharUUID.String())
 
+	reader := &transport.ReadHandler{}
+	writer := transport.NewWriteHandler(func(uuid, content []byte) {
+		req := &request{}
+		json.Unmarshal(content, req)
+		current := getState()
+
+		if req.RefreshOnly {
+			bytes, err := json.Marshal(current)
+			if err != nil {
+				bytes = []byte(err.Error())
+			}
+			reader = transport.NewReadHandler(uuid, bytes)
+			return
+		}
+
+		// check WIFIs
+		if len(req.WIFIs) != len(current.WIFIs) {
+			setWifiInfo(req.WIFIs)
+		} else {
+			for i := range req.WIFIs {
+				if req.WIFIs[i].SSID != current.WIFIs[i].SSID || req.WIFIs[i].PSK != "" {
+					setWifiInfo(req.WIFIs)
+					break
+				}
+			}
+		}
+
+		// check cancel static ip
+		if len(req.StaticIP) == 0 && len(current.StaticIP) != 0 {
+			cancelStaticIp(true)
+		}
+
+		// check set new static ip
+		if len(req.StaticIP) != 0 && (req.StaticIP != current.StaticIP || req.Router != current.Router) {
+			setNewStaticIP(req.StaticIP, req.Router)
+		}
+	})
+
 	return bluetooth.CharacteristicConfig{
 		UUID:  wifiCharBleUUID,
 		Flags: bluetooth.CharacteristicWritePermission | bluetooth.CharacteristicReadPermission,
 		ReadEvent: func(client bluetooth.Connection) ([]byte, error) {
-			return json.Marshal(getWifiInfo())
+			return reader.Read(), nil
 		},
 		WriteEvent: func(client bluetooth.Connection, offset int, value []byte) {
-			currentInfo := getWifiInfo()
-			newInfo := wifiInfo{}
-			json.Unmarshal(value, &newInfo)
-
-			if newInfo.SSID != currentInfo.SSID || newInfo.PSK != currentInfo.PSK {
-				setNewWifi(newInfo.SSID, newInfo.PSK)
-			}
-			if !newInfo.Static && currentInfo.Static {
-				cancelStaticIp(true)
-			}
-			if newInfo.Static && (newInfo.StaticIP != currentInfo.StaticIP || newInfo.Router != currentInfo.Router) {
-				setNewStaticIP(newInfo.StaticIP, newInfo.Router)
-			}
+			writer.Write(value)
 		},
 	}
 }
